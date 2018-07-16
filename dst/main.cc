8a21ac5b65795279ad9d867e0616e63c853fa486
liwen14 <liwen14@baidu.com>
2018-07-17 00:47:47 +0800

    first add main main.cc

--- dst/main.cc
+++ dst/main.cc
@@ -1,8 +1,23 @@
-#include <iostream> 
+#include "../bin/includes.h"
 
 
 #include <stdio.h>
 
 int main(){
     std::cout<<"hello world"<<std::endl;
+	std::fstream out("../dat/dat.txt");
+	if(!out.is_open())
+	{
+		std::cout<<"Error opening file";
+		std::cout<<std::endl;
+		exit(1);
+	}
+	char buffer[256];
+	while(!out.eof()){
+		out.getline(buffer,100);
+		std::cout<<buffer;
+	}
+	std::cout<<std::endl;
+	return 0;
+
 }
